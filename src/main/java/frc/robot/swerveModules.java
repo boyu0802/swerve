3a9da00558bba47c41c5976a4f90f582fd972599
boyu0802 <152668310+boyu0802@users.noreply.github.com>
2024-09-23 21:54:21 +0800

    first commit

---
+++ src/main/java/frc/robot/swerveModules.java
@@ -0,0 +1,11 @@
+package frc.robot;
+
+import com.revrobotics.CANSparkFlex;
+import com.revrobotics.CANSparkMax;
+
+public class swerveModules {
+    private static CANSparkMax angleMotor;
+    private static CANSparkFlex driveMotor;
+    private static 
+
+}
