3a90fe09389bb8dc8682e2bdbbe2235265f8fe64
boyu0802 <boyu0802chen@gmail.com>
2024-09-25 22:54:48 +0800

    skibidi toilet

--- src/main/java/frc/robot/swerveModules.java
+++ src/main/java/frc/robot/swerveModules.java
@@ -1,11 +1,41 @@
 package frc.robot;
 
+import com.ctre.phoenix6.hardware.CANcoder;
 import com.revrobotics.CANSparkFlex;
+import com.revrobotics.CANSparkLowLevel;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkPIDController;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Rotation2d;
+
+import frc.robot.Constants.swerveDriveConstants;
+import frc.robot.lib.*;
+
 
 public class swerveModules {
-    private static CANSparkMax angleMotor;
-    private static CANSparkFlex driveMotor;
-    private static 
+    private int moduleNumber;
+    private CANSparkMax angleMotor;
+    private CANSparkFlex driveMotor;
+    private CANcoder cancoder;
+    private RelativeEncoder driveEncoder;
+    private RelativeEncoder angleRelativeEncoder;
+    private Rotation2d lastAngle;
+    private Rotation2d angleOffset;
+    private SparkPIDController drivePID;
+    private SparkPIDController anglePID;
+    private SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(swerveDriveConstants.kS, swerveDriveConstants.kV,swerveDriveConstants.kA);
+    
+    
+
+    public swerveModules(int moduleNumber, swerveModuleConstants swerveModuleConstants ){
+        this.moduleNumber = moduleNumber;
+        angleMotor = new CANSparkMax(driveMotorID, MotorType.kBrushless);
+        driveMotor = new CANSparkFlex(driveMotorID, MotorType.kBrushless);
+        cancoder = new CANcoder(cancoderID);
+        
+    }
 
 }
